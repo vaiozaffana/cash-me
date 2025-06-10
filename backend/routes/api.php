f763ccf7d5f89371610d1028c27300495adde814
barastrong <145325312+barastrong@users.noreply.github.com>
2025-06-10 11:25:50 +0700

    update Route & Controller UserController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,4 +13,4 @@ Route::post('/users/store', [UserController::class, 'register']);
 Route::get('/users/show/{id}', [UserController::class, 'show']);
 Route::delete('/users/delete/{id}', [UserController::class, 'destroy']);
 Route::put('/users/update/{id}', [UserController::class, 'update']);
-Route::post('/login', [UserController::class, 'login']);
+// Route::post('/login', [UserController::class, 'login']);
